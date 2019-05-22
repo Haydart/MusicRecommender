0173d75be468715f0155486c3ed56703bb677cd6
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-22 02:31:43 +0200

    Add real genre color 3D scatter plot in html

--- every_noise/everynoisescraper/spiders/every_noise_spider.py
+++ every_noise/everynoisescraper/spiders/every_noise_spider.py
@@ -11,11 +11,12 @@ class ExampleSpider(scrapy.Spider):
     name = 'everynoise'
     allowed_domains = ['everynoise.com']
     start_urls = ['http://everynoise.com/']
+    genre_index = 0
 
     def parse(self, response):
         items = []
 
-        for container in response.xpath('//div[@class="genre scanme"]'):
+        for index, container in enumerate(response.xpath('//div[@class="genre scanme"]'), 1):
             name = container.xpath('./text()').extract()[0]
             link = container.xpath('./a[@class="navlink"]/@href').extract()[0]
 
@@ -39,77 +40,62 @@ class ExampleSpider(scrapy.Spider):
             item['font_size'] = font_size
 
             items.append(item)
-        print(items)
-        self.plot_3d_color_scatter(items)
 
-        # yield Request(url=urljoin(response.url, str(link)),
-        #               callback=self.parse_genre_site,
-        #               meta={"item": item})
+            yield Request(url=urljoin(response.url, str(link)),
+                          callback=self.parse_genre_site,
+                          meta={"item": item, "index": index})
+        # self.plot_3d_color_scatter(items)
 
     def parse_genre_site(self, response):
+        self.genre_index += 1
+        print(f'Processing genre {self.genre_index} - {response.meta["item"]["name"]}')
         x = response.meta['item']
-        print(x)
+        pass
 
     def extract_rgb(self, hex_color):
         stripped = hex_color.lstrip('#')
         return tuple(int(stripped[i:i + 2], 16) for i in (0, 2, 4))
 
-    def plot_3d_color_scatter(self, items_colors):
-        pass
+    def plot_3d_color_scatter(self, items):
+        names = [item['name'] for item in items]
+        x = [item['r'] for item in items]
+        y = [item['g'] for item in items]
+        z = [item['b'] for item in items]
 
-
-x, y, z = np.random.multivariate_normal(np.array([0, 0, 0]), np.eye(3), 200).transpose()
-print(x)
-trace1 = go.Scatter3d(
-    x=x,
-    y=y,
-    z=z,
-    mode='markers',
-    marker=dict(
-        size=12,
-        line=dict(
-            color='rgba(217, 217, 217, 0.14)',
-            width=0.5
-        ),
-        opacity=0.8
-    )
-)
-
-x2, y2, z2 = np.random.multivariate_normal(np.array([0, 0, 0]), np.eye(3), 200).transpose()
-trace2 = go.Scatter3d(
-    x=x2,
-    y=y2,
-    z=z2,
-    text=x2,
-    mode='markers',
-    marker=dict(
-        color='rgb(127, 127, 127)',
-        size=12,
-        symbol='circle',
-        line=dict(
-            color='rgb(204, 204, 204)',
-            width=1
-        ),
-        opacity=0.9
-    )
-)
-data = [trace1, trace2]
-layout = go.Layout(
-    title='Planets!',
-    scene=dict(xaxis=dict(title='Red',
-                          titlefont=dict(color='Black')),
-               yaxis=dict(title='Green',
-                          titlefont=dict(color='Black')),
-               zaxis=dict(title='Blue',
-                          titlefont=dict(color='Black')),
-               bgcolor='rgb(255, 255, 255)'
-               ),
-    margin=dict(
-        l=0,
-        r=0,
-        b=0,
-        t=0
-    )
-)
-fig = go.Figure(data=data, layout=layout)
-plotly.offline.plot(fig, auto_open=True)
+        trace2 = go.Scatter3d(
+            x=x,
+            y=y,
+            z=z,
+            text=names,
+            mode='markers',
+            marker=dict(
+                color=[f'rgb({item["r"]}, {item["g"]}, {item["b"]})' for item in items],
+                size=5,
+                symbol='circle',
+                line=dict(
+                    color='rgb(200, 200, 200)',
+                    width=1
+                ),
+                opacity=0.9
+            )
+        )
+        data = [trace2]
+        layout = go.Layout(
+            title='Planets!',
+            scene=dict(xaxis=dict(title='Red',
+                                  titlefont=dict(color='Black')),
+                       yaxis=dict(title='Green',
+                                  titlefont=dict(color='Black')),
+                       zaxis=dict(title='Blue',
+                                  titlefont=dict(color='Black')),
+                       bgcolor='rgb(255, 255, 255)'
+                       ),
+            margin=dict(
+                l=0,
+                r=0,
+                b=0,
+                t=0
+            )
+        )
+        fig = go.Figure(data=data, layout=layout)
+        plotly.offline.plot(fig, auto_open=True)
