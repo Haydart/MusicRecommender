fc1c82a8e2975f465550b4e0c5c3e0f14c6813ca
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-22 04:44:25 +0200

    Implement separate exporters for genres and artists

--- every_noise/everynoisescraper/pipelines.py
+++ every_noise/everynoisescraper/pipelines.py
@@ -4,16 +4,30 @@ from .items import EveryNoiseGenreItem, EveryNoiseArtistItem
 
 class EveryNoisePipeline(object):
 
+    def __init__(self):
+        self.genre_exporter = None
+        self.artist_exporter = None
+
+    def open_spider(self, spider):
+        self.genre_exporter = CsvItemExporter(file=open('genres_output.csv', 'wb'))
+        self.artist_exporter = CsvItemExporter(file=open('artists_output.csv', 'wb'))
+
+    def close_spider(self, spider):
+        self.genre_exporter.finish_exporting()
+        self.artist_exporter.finish_exporting()
+        self.genre_exporter.file.close()
+        self.artist_exporter.file.close()
+
     def process_item(self, item, spider):
         if isinstance(item, EveryNoiseGenreItem):
-            return self.handle_genre_item(item, spider)
+            return self.export_genre_item(item, spider)
         if isinstance(item, EveryNoiseArtistItem):
-            return self.handle_artist_item(item, spider)
+            return self.export_artist_item(item, spider)
 
-    def handle_genre_item(self, item, spider):
-        print("HANDLING GENRE ITEM")
+    def export_genre_item(self, item, spider):
+        self.genre_exporter.export_item(item)
         return item
 
-    def handle_artist_item(self, item, spider):
-        print("ARTIST")
+    def export_artist_item(self, item, spider):
+        self.artist_exporter.export_item(item)
         return item
