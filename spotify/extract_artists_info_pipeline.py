a523c594ec58310f0d4414c790a0ae6bc4accc26
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-27 16:35:17 +0200

    Deduplicate album names

--- spotify/extract_artists_info_pipeline.py
+++ spotify/extract_artists_info_pipeline.py
@@ -11,10 +11,10 @@ client = SpotifyClient()
 artist_id = artists_df.loc[artists_df['name'] == 'Disturbed'].iloc[0]['spotify_artist_id']
 print(f'artist id: {artist_id}')
 print(client.fetch_basic_artist_info(artist_id))
-album_names, album_uris = client.fetch_artist_albums(artist_id)
+album_names, album_uris = client.fetch_artist_albums(artist_id, filter_albums=True)
 print(album_names)
 print(album_uris)
 # print(client.fetch_album_songs(album_uris[2], album_names[2]))
 print("fetching albums info")
-print(client.fetch_albums_data(album_uris, filter_albums=True, filter_tracks=True))
+print(client.fetch_albums_data(album_uris))
 
--- spotify/spotify_client.py
+++ spotify/spotify_client.py
@@ -1,11 +1,8 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import time
-import numpy as np
-import pandas as pd
 import spotipy
 import os
 from spotipy.oauth2 import SpotifyClientCredentials
+from collections import Counter
+from .utils import list_duplicate_indices
 
 
 class SpotifyClient:
@@ -19,13 +16,30 @@ class SpotifyClient:
     def fetch_basic_artist_info(self, artist_uri):
         return self.spotipy.artist(artist_uri)
 
-    def fetch_artist_albums(self, artist_uri):
-        sp_albums = self.spotipy.artist_albums(artist_uri, album_type='album')
+    def fetch_artist_albums(self, artist_uri, filter_albums=True):
+        response = self.spotipy.artist_albums(artist_uri, album_type='album')
+        print(response)
         album_names = []
         album_uris = []
-        for i in range(len(sp_albums['items'])):
-            album_names.append(sp_albums['items'][i]['name'])
-            album_uris.append(sp_albums['items'][i]['uri'])
+        for i in range(len(response['items'])):
+            album_names.append(response['items'][i]['name'])
+            album_uris.append(response['items'][i]['uri'])
+
+        if filter_albums:
+            return self.__filter_duplicated_albums(album_names, album_uris)
+        else:
+            return album_names, album_uris
+
+    def __filter_duplicated_albums(self, album_names, album_uris):
+        while any(tup[1] > 1 for tup in Counter(album_names).most_common()): # while there are some duplicated album names
+            duplicates_list = [list_duplicate_indices(album_names, item) for item in album_names]
+            index_to_drop = next(duplicate[0] for duplicate in duplicates_list if len(duplicate) > 1)
+            print(duplicates_list)
+            print(index_to_drop)
+            del album_names[index_to_drop]
+            del album_uris[index_to_drop]
+        print("DEDU", album_names)
+        print("DEDU", album_uris)
         return album_names, album_uris
 
     def fetch_album_songs(self, album_uri, album_name):
@@ -41,15 +55,14 @@ class SpotifyClient:
             spotify_album['uri'].append(tracks['items'][n]['uri'])
         return spotify_album
 
-    def fetch_albums_data(self, album_uri_list, filter_albums=True, filter_tracks=True):
+    def fetch_albums_data(self, album_uri_list):
         if len(album_uri_list) > 20:
             raise ConnectionRefusedError('Cannot process more than 20 albums at once')
         response = self.spotipy.albums(album_uri_list)
-        names_to_uris = [{album['name']: album['uri']} for album in response['albums']]
 
-
-        tracks = [[{track['name'] : track['uri']} for track in album['tracks']['items']] for album in response['albums']]
-        return names_to_uris, tracks
+        albums = [(album['name'], album['uri']) for album in response['albums']]
+        tracks = [[(track['name'], track['uri']) for track in album['tracks']['items']] for album in response['albums']]
+        return albums, tracks
 
     def fetch_audio_features(self, track_uri_list):
         pass
