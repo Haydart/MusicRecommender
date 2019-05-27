b595df3b44554cc0bc0b8e3a7f64b55535656e05
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-27 16:35:30 +0200

    Add duplication listing util

--- spotify/util.py
+++
@@ -1,3 +0,0 @@
-def __chunk_spotify_track_uri_list(track_uri_list, limit=50):
-    for i in range(0, len(track_uri_list), limit):
-        yield track_uri_list[i:i + limit]
---
+++ spotify/utils.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+
+def chunk_spotify_track_uri_list(track_uri_list, limit=50):
+    for i in range(0, len(track_uri_list), limit):
+        yield track_uri_list[i:i + limit]
+
+
+def list_duplicate_indices(sequence, item):
+    return [i for i, x in enumerate(sequence) if x == item]
