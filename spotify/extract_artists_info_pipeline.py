7372040de701679187018a11333f298b3573b4d3
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-27 14:27:53 +0200

    Add album info extraction methods

---
+++ spotify/extract_artists_info_pipeline.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from spotify.spotify_client import SpotifyClient
+
+
+artists_df = pd.read_csv("../output/artists_output.csv")
+print(artists_df.head())
+print(f'Overall artist-genre pairs: {len(artists_df.index)}')
+print(f'Unique artists: {len(artists_df["spotify_artist_id"].unique())}')
+
+client = SpotifyClient()
+
+artist_id = artists_df.loc[artists_df['name'] == 'Disturbed'].iloc[0]['spotify_artist_id']
+print(f'artist id: {artist_id}')
+print(client.fetch_basic_artist_info(artist_id))
+album_names, album_uris = client.fetch_artist_albums(artist_id)
+print(album_names)
+print(album_uris)
+print(client.fetch_album_songs(album_uris[2], album_names[2]))
--- spotify/spotify_client.py
+++ spotify/spotify_client.py
@@ -7,6 +7,7 @@ import spotipy
 import os
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
 class SpotifyClient:
 
     def __init__(self):
@@ -15,7 +16,44 @@ class SpotifyClient:
         client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
         self.spotipy = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    def fetch_artist(self, spotify_artist_id):
-        return self.spotipy.artist(spotify_artist_id)
+    def fetch_basic_artist_info(self, artist_uri):
+        return self.spotipy.artist(artist_uri)
 
+    def fetch_artist_albums(self, artist_uri):
+        sp_albums = self.spotipy.artist_albums(artist_uri, album_type='album')
+        album_names = []
+        album_uris = []
+        for i in range(len(sp_albums['items'])):
+            album_names.append(sp_albums['items'][i]['name'])
+            album_uris.append(sp_albums['items'][i]['uri'])
+        return album_names, album_uris
 
+    def fetch_album_songs(self, album_uri, album_name):
+        spotify_album = {'album': [], 'track_number': [], 'id': [], 'name': [], 'uri': []}
+        tracks = self.spotipy.album_tracks(album_uri)
+        print(tracks)
+
+        for n in range(len(tracks['items'])):  # for each song track
+            spotify_album['album'].append(album_name)  # append album name tracked via album_count
+            spotify_album['track_number'].append(tracks['items'][n]['track_number'])
+            spotify_album['id'].append(tracks['items'][n]['id'])
+            spotify_album['name'].append(tracks['items'][n]['name'])
+            spotify_album['uri'].append(tracks['items'][n]['uri'])
+        return spotify_album
+
+    def fetch_audio_features(self, track_uri_list):
+        pass
+
+    def __chunk_spotify_track_uri_list(self, track_uri_list, limit=50):
+        for i in range(0, len(track_uri_list), limit):
+            yield track_uri_list[i:i + limit]
+
+    def get_albums_by_artist_from_list(self, artist_names):
+        artists_album_names = {}
+        artists_album_uris = {}
+        for artist in artist_names:
+            album_names, album_uris = self.fetch_artist_albums(artist)
+            if album_names is not None and album_uris is not None:
+                artists_album_names[artist] = album_names
+                artists_album_uris[artist] = album_uris
+        return artists_album_names, artists_album_uris
