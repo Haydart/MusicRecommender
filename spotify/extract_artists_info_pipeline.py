48d8b4762514e9a82b4d7f2eeec0f687816030e3
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-27 18:49:05 +0200

    Remove subset album names

--- spotify/extract_artists_info_pipeline.py
+++ spotify/extract_artists_info_pipeline.py
@@ -1,18 +1,23 @@
 import pandas as pd
 from spotify.spotify_client import SpotifyClient
 
-artists_df = pd.read_csv("../output/artists_output.csv")
+artists_df = pd.read_csv("../output/every_noise_artists_output.csv")
 print(artists_df.head())
 print(f'Overall artist-genre pairs: {len(artists_df.index)}')
 print(f'Unique artists: {len(artists_df["spotify_artist_id"].unique())}')
 
 client = SpotifyClient()
 
-artist_id = artists_df.loc[artists_df['name'] == 'Disturbed'].iloc[0]['spotify_artist_id']
-print(f'artist id: {artist_id}')
-print(client.fetch_basic_artist_info(artist_id))
-albums_names, albums_uris = client.fetch_artist_albums(artist_id, filter_albums=True)
-albums_tracks = client.fetch_albums_data(albums_uris)
+artist_ids = [artists_df.loc[artists_df['name'] == 'Disturbed'].iloc[0]['spotify_artist_id'],
+              artists_df.loc[artists_df['name'] == 'Avenged Sevenfold'].iloc[0]['spotify_artist_id'],
+              artists_df.loc[artists_df['name'] == 'Dream Theater'].iloc[0]['spotify_artist_id'],
+              artists_df.loc[artists_df['name'] == 'Saor'].iloc[0]['spotify_artist_id'],
+              artists_df.loc[artists_df['name'] == 'Mgła'].iloc[0]['spotify_artist_id']]
+
+print(f'artist id: {artist_ids[0]}')
+print(client.fetch_basic_artist_info(artist_ids))
+albums_names, albums_uris = client.fetch_artist_albums(artist_ids[0], filter_duplicate_names=True)
+albums_tracks = client.fetch_albums_tracks(albums_uris)
 print(albums_names)
 print(albums_uris)
 print(albums_tracks)
--- spotify/spotify_client.py
+++ spotify/spotify_client.py
@@ -1,5 +1,5 @@
-import spotipy
 import os
+import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from collections import Counter
 from .utils import list_duplicate_indices
@@ -13,10 +13,10 @@ class SpotifyClient:
         client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
         self.spotipy = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-    def fetch_basic_artist_info(self, artist_uri):
-        return self.spotipy.artist(artist_uri)
+    def fetch_basic_artist_info(self, artist_uris):
+        return self.spotipy.artists(artist_uris)
 
-    def fetch_artist_albums(self, artist_uri, filter_albums=True):
+    def fetch_artist_albums(self, artist_uri, filter_duplicate_names=True, filter_subset_albums=True):
         response = self.spotipy.artist_albums(artist_uri, album_type='album')
         album_names = []
         album_uris = []
@@ -24,8 +24,13 @@ class SpotifyClient:
             album_names.append(response['items'][i]['name'])
             album_uris.append(response['items'][i]['uri'])
 
-        if filter_albums:
-            return self.__filter_duplicated_albums(album_names, album_uris)
+        if filter_duplicate_names:
+            unique_album_names, unique_album_uris = self.__filter_duplicated_albums(album_names, album_uris)
+
+            if filter_subset_albums:
+                return self.__filter_subset_albums(unique_album_names, album_uris)
+            else:
+                return unique_album_names, unique_album_uris
         else:
             return album_names, album_uris
 
@@ -37,24 +42,39 @@ class SpotifyClient:
             del album_uris[index_to_drop]
         return album_names, album_uris
 
-    def fetch_album_songs(self, album_uri, album_name):
-        spotify_album = {'album': [], 'track_number': [], 'id': [], 'name': [], 'uri': []}
-        tracks = self.spotipy.album_tracks(album_uri)
-        print(tracks)
+    def __filter_subset_albums(self, albums_names, albums_uris):
+        album_names_to_drop = [checked_album_name for checked_album_name in albums_names for album_name in
+                               [album_name for album_name in albums_names] if
+                               checked_album_name in album_name and checked_album_name is not album_name]
 
-        for n in range(len(tracks['items'])):  # for each song track
-            spotify_album['album'].append(album_name)  # append album name tracked via album_count
-            spotify_album['track_number'].append(tracks['items'][n]['track_number'])
-            spotify_album['id'].append(tracks['items'][n]['id'])
-            spotify_album['name'].append(tracks['items'][n]['name'])
-            spotify_album['uri'].append(tracks['items'][n]['uri'])
-        return spotify_album
+        album_indices_to_drop = [albums_names.index(album_name) for album_name in album_names_to_drop]
+        album_uris_to_drop = [albums_uris[index] for index in album_indices_to_drop]
 
-    def fetch_albums_data(self, album_uri_list):
+        for album_name in album_names_to_drop:
+            albums_names.remove(album_name)
+
+        for album_uri in album_uris_to_drop:
+            albums_uris.remove(album_uri)
+
+        return albums_names, albums_uris
+
+    # def fetch_album_songs(self, album_uri, album_name):
+    #     spotify_album = {'album': [], 'track_number': [], 'id': [], 'name': [], 'uri': []}
+    #     tracks = self.spotipy.album_tracks(album_uri)
+    #     print(tracks)
+    #
+    #     for n in range(len(tracks['items'])):  # for each song track
+    #         spotify_album['album'].append(album_name)  # append album name tracked via album_count
+    #         spotify_album['track_number'].append(tracks['items'][n]['track_number'])
+    #         spotify_album['id'].append(tracks['items'][n]['id'])
+    #         spotify_album['name'].append(tracks['items'][n]['name'])
+    #         spotify_album['uri'].append(tracks['items'][n]['uri'])
+    #     return spotify_album
+
+    def fetch_albums_tracks(self, album_uri_list):
         if len(album_uri_list) > 20:
             raise ConnectionRefusedError('Cannot process more than 20 albums at once')
         response = self.spotipy.albums(album_uri_list)
-
         albums = [(album['name'], album['uri']) for album in response['albums']]
         tracks = [[(track['name'], track['uri']) for track in album['tracks']['items']] for album in response['albums']]
         return tracks
