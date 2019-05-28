47e287cd087760bcf666d55d6e16f0d9ec3c9dbe
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-29 01:35:31 +0200

    Run batched album tracks retrieval for each album

--- spotify/extract_tracks_uris_pipeline.py
+++ spotify/extract_tracks_uris_pipeline.py
@@ -17,6 +17,10 @@ albums_uris_nd = albums_df['album_uri'].values
 
 
 for i in range(0, len(albums_df.index), album_count_limit):
-
+    index = index + 1
+    if index % 100 == 0:
+        print(f'DONE {index} OUT OF {iterations_count} ALBUM BATCHES\t{time.ctime(int(time.time()))}')
+    albums_uris_batch = albums_uris_nd[i:min(i + album_count_limit, len(albums_df.index)), ]
+    tracks = client.fetch_albums_tracks(albums_uris_batch)
     # print(tracks, '\n')
 
