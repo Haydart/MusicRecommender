1875ff5b71c58884a42fc8ed36ec7747c86b5984
Haydart <makowiecki.radoslaw@gmail.com>
2019-05-29 01:35:01 +0200

    Iterate over nd values od album ids

--- spotify/extract_tracks_uris_pipeline.py
+++ spotify/extract_tracks_uris_pipeline.py
@@ -1,9 +1,22 @@
 import pandas as pd
 from spotify.spotify_client import SpotifyClient
+import time
 
 albums_df = pd.read_csv("../output/spotify_artists_albums_uris_output.csv")
 print(f'{len(albums_df)} albums before deduplication')
 albums_df = albums_df.drop_duplicates(subset=['album_uri'])
 print(f'{len(albums_df)} albums after deduplication')
 
+album_count_limit = 20
 client = SpotifyClient()
+result = []
+index = 0
+iterations_count = int(len(albums_df.index)/album_count_limit)
+
+albums_uris_nd = albums_df['album_uri'].values
+
+
+for i in range(0, len(albums_df.index), album_count_limit):
+
+    # print(tracks, '\n')
+
